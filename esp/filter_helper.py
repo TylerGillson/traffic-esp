522f38006d91a6c33a25ba4d00d6bed82b575c3f
cantyg <tyler.gillson@sas.com>
2019-09-12 22:38:22 -0400

    Add UK scraping branch

--- esp/filter_helper.py
+++ esp/filter_helper.py
@@ -34,19 +34,23 @@ traffic_keywords = [
 # Cities are ordered from largest to smallest, descending.
 # bboxes generated via: https://boundingbox.klokantech.com/
 bounding_boxes = [
-    (-74, 40, -73, 41),                # New York City, NY
-    (-118.67, 33.70, -118.16, 34.34),  # Los Angeles, CA
-    (-87.94, 41.64, -87.52, 42.02),    # Chicago, IL
-    (-95.91, 29.54, -95.01, 30.11),    # Houston, TX
-    (-112.67, 33.13, -111.37, 33.89),  # Phoenix, AZ
-    (-75.28, 39.87, -74.96, 40.14),    # Philadelphia, PA
-    (-98.81, 29.19, -98.22, 29.73),    # San Antonio, TX
-    (-117.31, 32.53, -116.91, 33.11),  # San Diego, CA
-    (-97, 32.61, -96.46, 33.02),       # Dallas, TX
-    (-122.09, 37.20, -121.73, 37.46),  # San Jose, CA
-    (-97.94, 30.10, -97.56, 30.52),    # Austin, TX
-    (-82.05, 30.10, -81.32, 30.59),    # Jacksonville, FL
-    (-97.59, 32.55, -97.03, 33.05),    # Fort Worth, TX
-    (-122.75, 36.8, -121.75, 37.8),    # San Francisco, CA
-    (-83.21, 39.81, -82.77, 40.12)     # Columbus, OH
+    (-11.69, 49.87, 1.85, 61.26),      # United Kingdom
 ]
+
+"""
+(-74, 40, -73, 41),                # New York City, NY
+(-118.67, 33.70, -118.16, 34.34),  # Los Angeles, CA
+(-87.94, 41.64, -87.52, 42.02),    # Chicago, IL
+(-95.91, 29.54, -95.01, 30.11),    # Houston, TX
+(-112.67, 33.13, -111.37, 33.89),  # Phoenix, AZ
+(-75.28, 39.87, -74.96, 40.14),    # Philadelphia, PA
+(-98.81, 29.19, -98.22, 29.73),    # San Antonio, TX
+(-117.31, 32.53, -116.91, 33.11),  # San Diego, CA
+(-97, 32.61, -96.46, 33.02),       # Dallas, TX
+(-122.09, 37.20, -121.73, 37.46),  # San Jose, CA
+(-97.94, 30.10, -97.56, 30.52),    # Austin, TX
+(-82.05, 30.10, -81.32, 30.59),    # Jacksonville, FL
+(-97.59, 32.55, -97.03, 33.05),    # Fort Worth, TX
+(-122.75, 36.8, -121.75, 37.8),    # San Francisco, CA
+(-83.21, 39.81, -82.77, 40.12)     # Columbus, OH
+"""
